235639a18a0802c050eaaa8187ea7e7de3fd795c
jlbuild <juliabuildbot@gmail.com>
2022-08-01 21:36:42 +0000

    libiio_jll build 0.24.0+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -5,12 +5,12 @@ using libusb_jll
 using XML2_jll
 using libaio_jll
 JLLWrappers.@generate_wrapper_header("libiio")
-JLLWrappers.@declare_library_product(libiio, "@rpath/iio")
+JLLWrappers.@declare_library_product(libiio, "@rpath/libiio.0.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(libusb_jll, XML2_jll, libaio_jll)
     JLLWrappers.@init_library_product(
         libiio,
-        "lib/iio.framework/Versions/0.23/iio",
+        "lib/libiio.0.24.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
